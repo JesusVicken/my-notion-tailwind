d99c0acc99213d136c5b6580526ef30e928ee966
JesusVicken <jesus.viccken@gmail.com>
2024-07-29 08:33:14 -0300

    tailwind installation and libs used in the project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+
+
+  return (
+    <div className="min-h-screen p-8 text-zinc-900 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+      <div className="bg-white w-[1100px] mx-auto rounded-xl min-h-[200px] shadow-sm border border-black/20">
+        <aside className="bg-zinc-50 border-r-zinc-100 p-4"></aside>
+        <main></main>
+      </div>
+    </div>
+  )
+}
+
+export default App
