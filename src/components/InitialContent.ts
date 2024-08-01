4fcba82a2335ff2c462125cc9d416a2b0b372085
JesusVicken <jesus.viccken@gmail.com>
2024-08-01 00:18:16 -0300

    feat: liberando notion

--- src/components/InitialContent.ts
+++ src/components/InitialContent.ts
@@ -1,52 +1,5 @@
 export const InitialContent = `
 
   <h1>Explorando Tipografia com Tailwind CSS</h1>
-  <p>
-    Este é um exemplo básico para testar o plugin de tipografia do Tailwind CSS. A tipografia é essencial para uma boa experiência de leitura, e o Tailwind oferece várias utilidades para estilizar textos de maneira eficaz.
-  </p>
-
-  <h2>Cabeçalhos e Parágrafos</h2>
-  <p>
-    O Tailwind CSS permite estilizar diferentes níveis de cabeçalhos, desde. Abaixo estão alguns exemplos:
-  </p>
-  <h3>Este é um cabeçalho H3</h3>
-  <p>
-    Este é um parágrafo simples. O Tailwind fornece classes para ajustar o espaçamento, tamanho da fonte, cor e muito mais. para alterar a cor do texto.
-  </p>
-  <h4>Este é um cabeçalho H4</h4>
-  <p>
-    Além disso, você pode adicionar para tornar o texto negrito ou para texto em itálico.
-  </p>
-
-  <h2>Listas</h2>
-  <p>Listas ordenadas e não ordenadas também são suportadas:</p>
-  <h3>Lista Ordenada</h3>
-  <ol>
-    <li>Primeiro item da lista</li>
-    <li>Segundo item da lista</li>
-    <li>Terceiro item da lista</li>
-  </ol>
-  <h3>Lista Não Ordenada</h3>
-  <ul>
-    <li>Primeiro item da lista</li>
-    <li>Segundo item da lista</li>
-    <li>Terceiro item da lista</li>
-  </ul>
-
-  <h2>Blocos de Citação</h2>
-  <ul>
-    <li>Primeiro item da lista</li>
-    <li>Segundo item da lista</li>
-    <li>Terceiro item da lista</li>
-  </ul>
-
-  <h2>Links</h2>
-  <a href="https://www.example.com">Visite nosso site</a>
-
-  <p>
-    Para incluir um trecho de código em uma frase, você pode usar a tag . Por exemplo, para exibir uma função JavaScript, você pode escrever:
-  </p>
-  <pre>
-  <code class="language-javascript">console.log("teste")</code>
-  </pre>
+  <p> Olá a todos, estou empolgado para compartilhar com vocês o meu espaço no Notion, onde mantenho todas as minhas ideias, projetos e anotações organizadas. Sinta-se à vontade para explorar, aprender e, se necessário, colaborar </p>
 `
